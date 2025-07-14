7a5dbc7bf1b66e46afea89908a4bf89e4c0c26e8
lucaspessoa1904 <lucaspessoa1904@gmail.com>
2025-07-14 18:32:40 -0300

    Create main.tsx

---
+++ main.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import FinalStep from './FinalStep';
+
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+root.render(
+  <React.StrictMode>
+    <FinalStep />
+  </React.StrictMode>
+);
